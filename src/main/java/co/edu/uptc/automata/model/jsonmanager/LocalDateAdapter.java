a2caf82ef8241f5543ca827ee1484273e8ca60b7
Darpi <iangmartinezt@gmail.com>
2026-04-02 23:01:18 -0500

    Metodos de LocalDateAdapter creados

--- src/main/java/co/edu/uptc/automata/model/jsonmanager/LocalDateAdapter.java
+++ src/main/java/co/edu/uptc/automata/model/jsonmanager/LocalDateAdapter.java
@@ -10,3 +10,9 @@ public class LocalDateAdapter implements JsonSerializer<LocalDate>, JsonDeserial
     public JsonElement serialize(LocalDate src, Type typeOfSrc, JsonSerializationContext context) {
         return new JsonPrimitive(src.format(DateTimeFormatter.ISO_LOCAL_DATE));
     }
+
+    @Override
+    public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        return LocalDate.parse(json.getAsString(), DateTimeFormatter.ISO_LOCAL_DATE);
+    }
+}
