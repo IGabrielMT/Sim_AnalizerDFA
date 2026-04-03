549724aef3fcc96bece8d5fd64d88c32585a5087
Darpi <iangmartinezt@gmail.com>
2026-04-02 22:31:43 -0500

    Transition Class Created

---
+++ src/main/java/co/edu/uptc/automata/model/automata/Transition.java
@@ -0,0 +1,12 @@
+package co.edu.uptc.automata.model.automata;
+
+import java.util.Map;
+
+public class Transition {
+    private Map<Character, String> transition;
+
+    public void Transition(Character character, String state){
+
+    }
+
+}
