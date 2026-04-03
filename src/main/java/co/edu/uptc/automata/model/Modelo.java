640c5e06adff65fd2f489fd416fcaf3b2de4b972
Darpi <iangmartinezt@gmail.com>
2026-04-02 23:19:36 -0500

    Clase Modelo creada

---
+++ src/main/java/co/edu/uptc/automata/model/Modelo.java
@@ -0,0 +1,16 @@
+package co.edu.uptc.automata.model;
+
+import co.edu.uptc.automata.model.automata.Automata;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Modelo {
+    private List<Automata> automatas;
+
+
+
+}
