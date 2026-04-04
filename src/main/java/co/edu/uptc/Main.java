a99a1354bc9b3014870a78d9f1191151703b0e3d
Darpi <iangmartinezt@gmail.com>
2026-04-03 23:05:24 -0500

    Flujo de inicio creado

--- src/main/java/co/edu/uptc/Main.java
+++ src/main/java/co/edu/uptc/Main.java
@@ -1,7 +1,18 @@
 package co.edu.uptc;
 
+import co.edu.uptc.automata.model.EvaluadorAutomatas;
+import co.edu.uptc.automata.model.Modelo;
+import co.edu.uptc.automata.model.automata.Automata;
+import co.edu.uptc.presenter.Presenter;
+import co.edu.uptc.view.View;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        Modelo modelo = new Modelo();
+        View view = new View();
+        Presenter presenter = new Presenter(modelo, view);
+        presenter.start();
     }
 }
