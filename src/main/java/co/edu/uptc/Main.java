46bd179e95b618ed76f0cc09b70dcee1ba18ec46
Darpi <iangmartinezt@gmail.com>
2026-04-03 23:06:54 -0500

    Imports borrados innecesarios

--- src/main/java/co/edu/uptc/Main.java
+++ src/main/java/co/edu/uptc/Main.java
@@ -1,8 +1,6 @@
 package co.edu.uptc;
 
-import co.edu.uptc.automata.model.EvaluadorAutomatas;
 import co.edu.uptc.automata.model.Modelo;
-import co.edu.uptc.automata.model.automata.Automata;
 import co.edu.uptc.presenter.Presenter;
 import co.edu.uptc.view.View;
 
